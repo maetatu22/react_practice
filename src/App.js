4e9dc5a8c97455fbc1ceb378ffff0ec43bfc73f4
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-18 19:19:50 +0900

    HelloWorld表示

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello React!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code>を編集したら保存する。
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <h1>Hello World</h1>
+    );
+  }
 }
 
 export default App;
