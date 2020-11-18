c60be94b883574d2ab7dba945439802cffb680b6
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-18 18:23:26 +0900

    日本語表記

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello React!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>を編集したら保存する。
         </p>
         <a
           className="App-link"
