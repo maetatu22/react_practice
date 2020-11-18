4ba730ad54da041af4d59edc24d54076114af133
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-18 19:37:18 +0900

    App.jsの構成練習

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-
 class App extends React.Component{
   render(){
-    return(
-      <div>
-        <h1>Hello World</h1>
-        <p>一緒にReactを学びましょう！</p>
-        <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ninjawanko.png" />
-      </div>
+    return (
+      <h1>Hello React</h1>
     );
   }
 }
