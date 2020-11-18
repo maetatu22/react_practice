4a2033db0df4cf7f2f3e6bee78f29e22ab5fdb16
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-18 22:09:15 +0900

    定数読み込み

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 class App extends React.Component{
   render(){
+    const name = "にんじゃわんこ";
+    const imgUrl = "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ninjawanko.png";
     return (
-      <h1>Hello React</h1>
+      <div>
+        <h1>{name}</h1>
+        <img src = {imgUrl} />
+      </div>
     );
   }
 }
